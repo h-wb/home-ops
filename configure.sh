86def68801bbf75eb9c717993415dd537cd6edc9
h-wb <dev.github@wlab.ovh>
2022-01-30 13:56:40 +0100

    feat: add secrets

--- configure.sh
+++ configure.sh
@@ -50,8 +50,11 @@ main() {
             > "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
         envsubst < "${PROJECT_DIR}/tmpl/cluster/cert-manager-secret.sops.yaml" \
             > "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
+        envsubst < "${PROJECT_DIR}/tmpl/cluster/external-dns-secret.sops.yaml" \
+            > "${PROJECT_DIR}/cluster/apps/networking/external-dns/secret.sops.yaml"
         sops --encrypt --in-place "${PROJECT_DIR}/cluster/base/cluster-secrets.sops.yaml"
         sops --encrypt --in-place "${PROJECT_DIR}/cluster/core/cert-manager/secret.sops.yaml"
+        sops --encrypt --in-place "${PROJECT_DIR}/cluster/apps/networking/external-dns/secret.sops.yaml"
         # terraform
         # envsubst < "${PROJECT_DIR}/tmpl/terraform/secret.sops.yaml" \
         #     > "${PROJECT_DIR}/provision/terraform/cloudflare/secret.sops.yaml"
